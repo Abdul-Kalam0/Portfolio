11e2cf59a7a2fd8db59d68969f5e6e4c3ed57cb9
Abdul-Kalam0 <abdulkalamblycomp@gmail.com>
2026-06-27 18:59:13 +0530

    Updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -64,7 +64,7 @@ export default function Contact() {
               d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
             />
           </svg>
-          Say Hello
+          Email Me
         </a>
 
         <div className="flex flex-col items-center gap-4 w-full max-w-2xl">
